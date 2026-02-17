b34a61695dc35f0e00e86a9ca8954ebc10c5d743
jriccostables <jriccostables@gmail.com>
2026-02-16 19:43:42 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Chancellor from './Chancellor.jsx'
+import Chancellor from './MrChancellor.jsx'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
